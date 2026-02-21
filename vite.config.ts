7f2940478f308b24323b9465890e19073504e65e
Karol Górnik <155629482+CodingMiner@users.noreply.github.com>
2026-02-21 19:46:47 +0100

    update configuration for Playwright and Vitest, add e2e testing setup and enhance .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/atm-machine/',
+  plugins: [tailwindcss(), react()],
+  test: {
+    environment: 'node',
+    include: ['src/**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      include: ['src/utils/**'],
+      reporter: ['text', 'lcov'],
+    },
+  },
 })
